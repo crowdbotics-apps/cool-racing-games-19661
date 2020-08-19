b065c40d513f93986547899b3a312d84a68f5d5a
Crowdbotics <team@crowdbotics.com>
2020-08-19 19:36:19 +0000

    Initial commit.

---
+++ ios/cool_racing_games_19661Tests/cool_racing_games_19661Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface cool_racing_games_19661 : XCTestCase
+
+@end
+
+@implementation cool_racing_games_19661
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
